4baf15123be7409c7d6457b61de12c3413d55eb5
Deepanshi <kathuriadeepanshi56@gmail.com>
2024-11-20 01:14:06 +0530

    if-else condition

---
+++ ifelse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // If-else
+
+    int age = 18;
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "Your are eligible to vote";
+    }
+    else
+    {
+        cout << "you are not eligible to vote";
+    }
+
+    return 0;
+}
