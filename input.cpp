f68228e488107d6a4f12503ea5b695d7ec0877c5
Deepanshi <kathuriadeepanshi56@gmail.com>
2024-11-19 19:36:22 +0530

    input in cpp

---
+++ input.cpp
@@ -0,0 +1,13 @@
+//  how to take input from user in cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age; 
+    cout<<"enter the age:";
+    cin >> age;
+
+    cout<<"your age is :" << age << endl;
+
+    return 0;
+}
